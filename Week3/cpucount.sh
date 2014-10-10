aca15c3b61b199993b4827661edc4fda76b13850
jzburns <jzburns@gmail.com>
2014-10-10 11:17:19 +0100

    this is another change

--- Week3/cpucount.sh
+++ Week3/cpucount.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Title : 
+# Date : 
+# Author : 
+# Version :
+# Description : 
+# Options : 
 
-#: Title : 
-#: Date : 
-#: Author : 
-#: Version :
-#: Description : 
-#: Options : 
+# use printf to print a message to the 
+# user eg The number of CPUs is
 
 grep processor /proc/cpuinfo | wc -l
